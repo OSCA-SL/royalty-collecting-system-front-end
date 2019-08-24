5d8ca4edc0c9861aa86564d0471db0f6cf6a082c
hemanthi <hemanthi@ncinga.com>
2019-08-24 22:18:06 +0530

    Temp commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,31 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import {RouterModule, Routes} from "@angular/router";
+import { HomeComponent } from './auth/home/home.component';
+
+const appRoutes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'home', component: HomeComponent }
+];
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule
   ],
   providers: [],
